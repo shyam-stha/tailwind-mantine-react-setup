1a9227801d40d032a2176d231f86e325c0be09b3
Shyam Stha <sthask2019@gmail.com>
2023-01-02 13:51:43 +0545

    Extends the default config with custom of tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js.ts.jsx.tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                roboto: 'Roboto',
+                fira: 'Fira Code',
+                popins: 'Poppins',
+                sans: ['Lato', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: '#1fb6ff',
+                seconday: '#ff7849',
+                error: '#FF0000',
+                warning: '#00FF00',
+            },
+            screens: {
+                sm: '640px',
+                md: '768px',
+                lg: '1024px',
+                xl: '1280px',
+                '2xl': '1536px',
+            },
+        },
+    },
+    plugins: [],
+}
