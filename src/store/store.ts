1f55ac91bcd7d0fc52b3958ffc18c0dc50b93dd2
Shyam Stha <sthask2019@gmail.com>
2023-01-02 14:34:27 +0545

    Add reducer to the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import authReducer from './authReducers/authSlice'
 
 export const store = configureStore({
-    reducer : {
-
-    }
+    reducer: {
+        auth: authReducer,
+    },
 })
 
-
 export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch
